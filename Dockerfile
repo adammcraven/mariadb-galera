37c3f5a38c6c164411ba47c8ea3ac9edf86ee067
Adam <Adam>
2015-11-25 21:51:20 +1000

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM mariadb:10.0
+
+RUN apt-get update && \
+    DEBIAN_FRONTEND=noninteractive apt-get install -y mariadb-galera-server && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+RUN mysql_install_db
+
+ADD galera.cnf /etc/mysql/conf.d/
+ADD run.sh /
+VOLUME /var/lib/mysql
+
+ENTRYPOINT ["/run.sh"]
+
+EXPOSE 3306 4567 4444
+CMD ["mysqld"]
---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+if [ "$NODE_IP" ]; then
+	echo "wsrep_node_address=$NODE_IP" >> /etc/mysql/conf.d/galera.cnf
+fi
+
+if [ "$MYSQL_CLUSTER_NAME" ]; then
+	echo"wsrep_cluster_name=$MYSQL_CLUSTER_NAME" >> /etc/mysql/conf.d/galera.cnf
+fi
+
+if [ "$CLUSTER_ADDRESS" ]; then
+	export MYSQL_ALLOW_EMPTY_PASSWORD="yes"
+fi
+
+echo "wsrep_cluster_address=${CLUSTER_ADDRESS-gcomm://}" >> /etc/mysql/conf.d/galera.cnf
+
+sed '/^[\t].mysqld/s/mysqld/"$@"/' -i /docker-entrypoint.sh
+sed '/mysql_tzinfo_to_sql/d' -i /docker-entrypoint.sh
+exec /docker-entrypoint.sh "$@"
