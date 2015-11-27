f9db8716318a68ed9c7741e4d6aedd3296446e9c
Adam Craven <adammcraven@gmail.com>
2015-11-28 00:05:09 +1000

    wip

--- run.sh
+++ run.sh
@@ -3,12 +3,15 @@ set -e
 
 configure_galera_config_file() {
   rm -f /etc/mysql/conf.d/galera-tmp.cnf
+  echo "[mysqld]" > /etc/mysql/conf.d/galera-tmp.cnf 
 
   if [ "$CLUSTER_START_MODE" = "new" ]; then
+    echo "==> Starting a new db cluster"
     echo "wsrep_new_cluster=true" >> /etc/mysql/conf.d/galera-tmp.cnf
   fi
 
   if [ "$CLUSTER_START_MODE" = "restart" ]; then
+    echo "==> Starting an existing db cluster"
     echo "TODO - restart CLUSTER_START_MODE"
     exit -1
   fi
@@ -29,61 +32,61 @@ configure_galera_config_file() {
 }
 
 create_custom_database() {
-  if [ $DB_DATABASE ]; then
-    echo "==> Creating database $DB_DATABASE..."
+  if [ $MYSQL_DATABASE ]; then
+    echo "==> Creating database $MYSQL_DATABASE..."
     echo ""
-    echo "CREATE DATABASE IF NOT EXISTS \`$DB_DATABASE\`;" >> /tmp/init_mysql.sql
+    echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\`;" >> /tmp/init_mysql.sql
   fi
 }
 
 create_user() {
 #  if [ "$DB_REPLICATION_MODE" == "slave" ]; then
-#    if [ ! "$DB_USER" ] || [ ! "${DB_PASSWORD}" ] || [ ! "$DB_DATABASE" ]; then
+#    if [ ! "$MYSQL_USER" ] || [ ! "${MYSQL_PASSWORD}" ] || [ ! "$MYSQL_DATABASE" ]; then
 #      echo "==> Trying to fetch MariaDB user/password from the master link..."
-#      DB_USER=${DB_USER:-$MASTER_ENV_DB_USER}
-#      DB_PASSWORD=${DB_PASSWORD:-$MASTER_ENV_DB_PASSWORD}
+#      MYSQL_USER=${MYSQL_USER:-$MASTER_ENV_MYSQL_USER}
+#      MYSQL_PASSWORD=${MYSQL_PASSWORD:-$MASTER_ENV_MYSQL_PASSWORD}
 #      DB_DATABASE=${DB_DATABASE:-$MASTER_ENV_DB_DATABASE}
 #    fi
 #  fi
 
-  if [ ! $DB_USER ]; then
-    DB_USER=root
+  if [ ! $MYSQL_USER ]; then
+    MYSQL_USER=root
   fi
 
-  if [ "$DB_USER" = "root" ] && [ ! $MYSQL_ROOT_PASSWORD ]; then
-    echo "In order to use a root DB_USER you need to provide the MYSQL_ROOT_PASSWORD as well"
+  if [ "$MYSQL_USER" = "root" ] && [ ! $MYSQL_ROOT_PASSWORD ]; then
+    echo "In order to use a root MYSQL_USER you need to provide the MYSQL_ROOT_PASSWORD as well"
     echo ""
     exit -1
   fi
 
-  if [ "$DB_USER" != "root" ] && [ ! $DB_DATABASE ]; then
-    echo "In order to use a custom DB_USER you need to provide the DB_DATABASE as well"
+  if [ "$MYSQL_USER" != "root" ] && [ ! $MYSQL_DATABASE ]; then
+    echo "In order to use a custom MYSQL_USER you need to provide the DB_DATABASE as well"
     echo ""
     exit -1
   fi
 
-  echo "==> Creating user $DB_USER..."
+  echo "==> Creating user $MYSQL_USER..."
   echo ""
 
   echo "DELETE FROM mysql.user ;" >> /tmp/init_mysql.sql
-  echo "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}' ;" >> /tmp/init_mysql.sql
+  echo "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}' ;" >> /tmp/init_mysql.sql
 
-  if [ "$DB_USER" = "root" ]; then
+  if [ "$MYSQL_USER" = "root" ]; then
     echo "==> Creating root user with unrestricted access..."
     echo "GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;" >> /tmp/init_mysql.sql
   else
-    echo "==> Granting access to $DB_USER to the database $DB_DATABASE..."
+    echo "==> Granting access to $MYSQL_USER to the database $MYSQL_DATABASE..."
     echo ""
-    echo "GRANT ALL ON \`${DB_DATABASE}\`.* TO \`${DB_USER}\`@'%' ;" >> /tmp/init_mysql.sql
-    echo "GRANT RELOAD, REPLICATION CLIENT ON *.* TO \`${DB_USER}\`@'%' ;" >> /tmp/init_mysql.sql
+    echo "GRANT ALL ON \`${DB_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' ;" >> /tmp/init_mysql.sql
+    echo "GRANT RELOAD, REPLICATION CLIENT ON *.* TO \`${MYSQL_USER}\`@'%' ;" >> /tmp/init_mysql.sql
   fi
 
   echo "FLUSH PRIVILEGES ;" >> /tmp/init_mysql.sql
-#  echo "DROP DATABASE IF EXISTS test ; " >> /tmp/init_mysql.sql
+  echo "DROP DATABASE IF EXISTS test ;" >> /tmp/init_mysql.sql
 }
 
 validate_db_database() {
-  if [ ! $DB_DATABASE ]; then
+  if [ ! $MYSQL_DATABASE ]; then
     echo "You need to provide the DB_DATABASE"
     echo ""
     exit -1
@@ -135,8 +138,8 @@ configure_replication() {
 
       echo "==> Trying to fetch MariaDB replication parameters from the master link..."
 #      DB_MASTER_HOST=${DB_MASTER_HOST:-$MASTER_PORT_3306_TCP_ADDR}
-#      DB_MASTER_USER=${DB_MASTER_USER:-$MASTER_ENV_MARIADB_USER}
-#      DB_MASTER_PASSWORD=${DB_MASTER_PASSWORD:-$MASTER_ENV_MARIADB_PASSWORD}
+#      DB_MASTER_USER=${DB_MASTER_USER:-$MASTER_ENV_MARIAMYSQL_USER}
+#      DB_MASTER_PASSWORD=${DB_MASTER_PASSWORD:-$MASTER_ENV_MARIAMYSQL_PASSWORD}
 #      DB_REPLICATION_USER=${DB_REPLICATION_USER:-$MASTER_ENV_MARIADB_REPLICATION_USER}
 #      DB_REPLICATION_PASSWORD=${DB_REPLICATION_PASSWORD:-$MASTER_ENV_MARIADB_REPLICATION_PASSWORD}
 
@@ -183,7 +186,7 @@ snapshot_master_data_for_slave() {
   
   echo "==> Creating a data snapshot..."
   mysqldump -u$DB_MASTER_USER ${DB_MASTER_PASSWORD:+-p$DB_MASTER_PASSWORD} -h $DB_MASTER_HOST \
-    --databases $DB_DATABASE --skip-lock-tables --single-transaction --flush-logs --hex-blob --master-data --apply-slave-statements --comments=false | tr -d '\012' | sed -e 's/;/;\n/g' >> /tmp/init_mysql.sql
+    --databases $MYSQL_DATABASE --skip-lock-tables --single-transaction --flush-logs --hex-blob --master-data --apply-slave-statements --comments=false | tr -d '\012' | sed -e 's/;/;\n/g' >> /tmp/init_mysql.sql
   echo ""
 }
 
@@ -191,12 +194,18 @@ snapshot_master_data_for_slave() {
 #############################################
 ################## Start ####################
 #############################################
-rm -f /tmp/init_mysql.sql
 
-create_custom_database
-create_user
+echo "==> Start of run.sh"
 
 configure_galera_config_file
+
+rm -f /tmp/init_mysql.sql
+
+echo "==> Running MariaDB Docker container's docker-entrypoint.sh"
+exec /docker-entrypoint.sh "$@"
+
+#create_custom_database
+#create_user
 configure_replication
 
 if [ "$DB_REPLICATION_MODE" = "slave" ]; then  
@@ -204,4 +213,4 @@ if [ "$DB_REPLICATION_MODE" = "slave" ]; then
   snapshot_master_data_for_slave
 fi
 
-exec /docker-entrypoint.sh "$@"
+echo "==> End of run.sh"
